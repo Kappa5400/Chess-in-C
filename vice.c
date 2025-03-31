8e6e4cae2b76719cff335d8b6b24bb7949be8477
kappa5400 <kappa6708@gmail.com>
2025-03-31 12:50:31 -0500

    Update

--- vice.c
+++ vice.c
@@ -4,33 +4,13 @@
 int main()  {
     
     AllInit();
-    
-    int index = 0;
-
-    for(index = 0; index < BRD_SQ_NUM; ++index) {
-        if(index%10==0) printf("\n");
-        printf("%5d",Sq120ToSq64[index]);
-    }
-
-    printf("\n");
-    printf("\n");
-
-    for(index = 0; index < 64; ++index) {
-        if(index%8==0) printf("\n");
-        printf("%5d",Sq64ToSq120[index]);
-    }
-
-    printf("\n\n");
 
     U64 playBitBoard = 0ULL;
 
-    printf("Start:\n\n");
-    PrintBitBoard(playBitBoard);
-
-    playBitBoard |= (1ULL<<SQ64(D2));
-
-    printf("D2 Added: \n\n");
-    PrintBitBoard(playBitBoard);
+    playBitBoard |= (1ULL << SQ64(D2));
+    playBitBoard |= (1ULL << SQ64(D3));
+    playBitBoard |= (1ULL << SQ64(D4));
+    
 
     return 0;
 }
