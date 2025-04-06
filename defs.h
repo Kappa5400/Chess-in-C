aba99ebb1c0eff36796b131cb9b0e72031e56323
Colin <kappa6708@gmail.com>
2025-04-06 17:08:49 -0500

    update

--- defs.h
+++ defs.h
@@ -95,12 +95,15 @@ typedef struct {
 #define SQ64(sq120) (Sq120ToSq64[(sq120)])
 #define POP(b) PopBit(b)
 #define CNT(b) CountBits(b)
+#define CRLBIT (bb,sq) ((bb) &= ClearMask[(sq)])
+#define SETBIT(bb,sq) ((bb) |= SetMask[(sq])])
 
 /* Globals*/
 
 extern int Sq120ToSq64[BRD_SQ_NUM];
 extern int Sq64ToSq120[64];
-
+extern U64 SetMask[64];
+extern U64 ClearMask[64];
 
 
 /* Functions*/
